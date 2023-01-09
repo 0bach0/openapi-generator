2db02a69f0822e8c239da8d77e6a55c24108bf04
Bach <xuan_bach_nguyen@earthbrain.com>
2023-01-09 19:40:59 +0900

    #11785 generate by new.sh

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/TypescriptNestjsServerCodegen.java
@@ -0,0 +1,46 @@
+package org.openapitools.codegen.languages;
+
+import org.openapitools.codegen.*;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.MapProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.models.parameters.Parameter;
+
+import java.io.File;
+import java.util.*;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TypescriptNestjsServerCodegen extends DefaultCodegen implements CodegenConfig {
+    public static final String PROJECT_NAME = "projectName";
+
+    static final Logger LOGGER = LoggerFactory.getLogger(TypescriptNestjsServerCodegen.class);
+
+    public CodegenType getTag() {
+        return CodegenType.SERVER;
+    }
+
+    public String getName() {
+        return "typescript-nestjs";
+    }
+
+    public String getHelp() {
+        return "Generates a typescript-nestjs server.";
+    }
+
+    public TypescriptNestjsServerCodegen() {
+        super();
+
+        outputFolder = "generated-code" + File.separator + "typescript-nestjs";
+        modelTemplateFiles.put("model.mustache", ".zz");
+        apiTemplateFiles.put("api.mustache", ".zz");
+        embeddedTemplateDir = templateDir = "typescript-nestjs";
+        apiPackage = "Apis";
+        modelPackage = "Models";
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        // TODO: Fill this out.
+    }
+}
---
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/typescript/nestjs/TypescriptNestjsServerCodegenTest.java
@@ -0,0 +1,19 @@
+package org.openapitools.codegen.typescript.nestjs;
+
+import org.openapitools.codegen.*;
+import org.openapitools.codegen.languages.TypescriptNestjsServerCodegen;
+import io.swagger.models.*;
+import io.swagger.parser.SwaggerParser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TypescriptNestjsServerCodegenTest {
+
+    TypescriptNestjsServerCodegen codegen = new TypescriptNestjsServerCodegen();
+
+    @Test
+    public void shouldSucceed() throws Exception {
+        // TODO: Complete this test.
+        Assert.fail("Not implemented.");
+    }
+}
